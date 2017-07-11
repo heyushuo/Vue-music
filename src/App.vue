4d1da2e0b556a11c6ef2336f2a54b863745cb31f
heyushuo <1007561607@qq.com>
2017-07-11 23:17:51 +0800

    配置和头部和导航

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <MHeader></MHeader>
+    <Tab></Tab>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
 export default {
-  name: 'app'
+  components:{
+  	MHeader,
+  	Tab
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+	
 </style>
